06c63b3330c31b28f54e802ff4897d54fdb75b65
Erik Geiser <erik.geiser@redteam-pentesting.de>
2025-08-20 15:48:24 +0200

    Add test

--- x509ext/sid_test.go
+++ x509ext/sid_test.go
@@ -3,6 +3,7 @@ package x509ext_test
 import (
 	"crypto/x509"
 	"crypto/x509/pkix"
+	"encoding/base64"
 	"testing"
 
 	"github.com/RedTeamPentesting/adauth/x509ext"
@@ -29,5 +30,28 @@ func TestSID(t *testing.T) {
 
 	if parsedSID != sid {
 		t.Fatalf("parsed SID %s instead of %s", parsedSID, sid)
+	}
+}
+
+func TestParseSID(t *testing.T) {
+	expectedSID := "S-1-5-21-3562467320-83266577-1006951523-1141"
+
+	testExtensionData, err := base64.StdEncoding.DecodeString(
+		"MD6gPAYKKwYBBAGCNxkCAaAuBCxTLTEtNS0yMS0zNTYyNDY3MzIwLTgzMjY2NTc3LTEwMDY5NTE1MjMtMTE0MQ==",
+	)
+	if err != nil {
+		t.Fatalf("decode test extension: %v", err)
+	}
+
+	sid, err := x509ext.SIDFromExtension(pkix.Extension{
+		Id:    x509ext.NTDSCASecurityExtOID,
+		Value: testExtensionData,
+	})
+	if err != nil {
+		t.Fatalf("SIDFromExtension: %v", err)
+	}
+
+	if sid != expectedSID {
+		t.Fatalf("extracted SID is %q instead of %q", sid, expectedSID)
 	}
 }
